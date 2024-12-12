d9be9d46d7078a8e5c16c8231d63f4cab361c3c5
Ricardo Satio Arita <ricardo0satio@gmail.com>
2024-12-11 21:40:37 -0300

    Update scriptsipag20.js

--- scriptsipag20.js
+++ scriptsipag20.js
@@ -116,6 +116,9 @@ window.onload = function () {
       case "25166191":
         skillFormatada = "25166191 - Sipag 2.0 Reativacao de Cadastro";
         break;
+      case "25166192":
+        skillFormatada = "25166192 - Sipag 2.0 Habilitar Servico Antecipacao";
+        break;
       case "23120887":
         skillFormatada = "23120887 - Sipag Erro Validação URA";
         break;
@@ -168,6 +171,9 @@ function showPopup() {
         break;
       case "25166191":
         transfSkill = "25166191 - Sipag 2.0 Reativacao de Cadastro";
+        break;
+      case "25166192":
+        transfSkill = "25166192 - Sipag 2.0 Habilitar Servico Antecipacao";
         break;
       default:
         transfSkill = " ";
@@ -230,6 +236,7 @@ const allOptions = [
   { value: "23120886", text: "23120886 - Sipag 2.0 Suporte Tecnico" },
   { value: "23121572", text: "23121572 - Sipag 2.0 Retencao" },
   { value: "25166191", text: "25166191 - Sipag 2.0 Reativacao de Cadastro" },
+  { value: "25166191", text: "25166192 - Sipag 2.0 Habilitar Servico Antecipacao" },
   { value: "PUC", text: "URA PUC" }
 ];
 
